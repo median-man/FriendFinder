ea7008447a8e3e4ff2fad9031ad2e087a2a44a6c
John Desrosiers <johndrunner@yahoo.com>
2017-09-23 09:21:08 -0700

    added data validation

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -16,13 +16,33 @@ module.exports = function(app) {
         res.json(friends);
     });
 
-    app.post("/api/friends", function(req, res) {
-        // TODO process post request (handle incoming survey results)
-        console.log(req);
+	// Return a json of match from friends and add user to friends
+	app.post("/api/friends", function(req, res) {
+		// get the friend object returned by the user
+		let user = req.body;
 
         friends.push(req.body);
 
-        // TODO add friend to data
-        res.json(req.body);
-    });
+		// validate the user data
+		try {
+	
+			// TODO test if script tags can be given as imput and subsequently executed when
+			// displayed in the browser
+			user.name = user.name.toString();
+			user.photo = user.photo.toString();
+			if ( user.name.length < 1 ) throw "Invalid user name. User name must have at least one character.";
+	
+			// validate and format the scores array
+			if ( user.scores.length !== 10 ) throw "Unexpected count of scores from the survey. There must be exactly 10 scores.";
+			user.scores = user.scores.map(function(score) {	
+				score = parseInt(score);
+				if ( score < 1 || score > 5 ) throw "Invalid score. Score must be between 1 and 5 inclusive.";
+				return score;
+			});
+		}
+		catch ( err ) {
+	
+		// TODO: handle user input error
+			return res.send(err.toString());
+		}
 };
